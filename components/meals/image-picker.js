75806269c1e19ad7a972bd3d9a0d073de1b9275d
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-08 17:43:53 +0000

    image-picker-preview

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,9 +1,11 @@
 'use client';
-
-import { useRef } from 'react';
+import Image from 'next/image';
+import { useRef, useState } from 'react';
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({label, name}) {
+
+    const [pickedImage, setPickedImage] = useState();
 
     const imageInput = useRef();
 
@@ -11,11 +13,41 @@ export default function ImagePicker({label, name}) {
         imageInput.current.click();
     }
 
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+
+        if(!file){
+            setPickedImage(null)
+            return;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
+    }
+
     return (
         <div className={classes.picker}>
             <label htmlFor={name}>{label}</label>
             <div className={classes.control}>
-                <input type="file" id={name} accept="image/png, image/jpeg" name={name} className={classes.input} ref={imageInput} />
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No Image picked yet.</p>}
+                    {pickedImage && <Image src={pickedImage} alt="The image selected by the user." fill />}
+                </div>
+                <input 
+                type="file" 
+                id={name} 
+                accept="image/png, image/jpeg" 
+                name={name} 
+                className={classes.input} 
+                ref={imageInput} 
+                onChange={handleImageChange}
+                required
+                />
                 <button className={classes.button} type="button" onClick={handlePickClick}>Pick an Image</button>
             </div>
         </div>
